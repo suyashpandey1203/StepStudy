f1d455b57ba8c15cac69a43917053cc4f90798ea
suyashpandey1203 <girijeshpandey.gp@gmail.com>
2025-04-25 23:20:07 +0530

    getInfoforId

--- server/controllers/BootcampNotification.js
+++ server/controllers/BootcampNotification.js
@@ -49,6 +49,9 @@ exports.respondToBootcampRequest = async (req, res) => {
 // Get notifications for logged-in user
 exports.getNotifications = async (req, res) => {
   try {
+    // if user opne notification, it will see all send to it
+
+    
     const notifications = await Notification.find({ receiver: req.user.id })
       .sort({ createdAt: -1 })
       .populate("sender", "firstName lastName email");
@@ -59,5 +62,27 @@ exports.getNotifications = async (req, res) => {
     });
   } catch (err) {
     res.status(500).json({ success: false, message: "Server error" });
+  }
+};
+const Notification = require("../models/Notification");
+
+exports.getNotificationsByReceiverId = async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const notifications = await Notification.find({ receiver: userId })
+      .sort({ createdAt: -1 })
+      .populate("sender", "firstName lastName email");
+
+    res.status(200).json({
+      success: true,
+      notifications,
+    });
+  } catch (error) {
+    console.error("Error fetching notifications:", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
   }
 };
--- server/routes/BootcampNotification.js
+++ server/routes/BootcampNotification.js
@@ -4,11 +4,12 @@ const {
   sendBootcampRequest,
   respondToBootcampRequest,
   getNotifications,
+  getNotificationsByReceiverId
 } = require("../controllers/BootcampNotification");
 const { auth } = require("../middlewares/auth");
 
 router.post("/request", auth, sendBootcampRequest);
 router.post("/respond", auth, respondToBootcampRequest);
 router.get("/my", auth, getNotifications);
-
+router.get("/:userId", getNotificationsByReceiverId);
 module.exports = router;
